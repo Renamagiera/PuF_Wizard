5a97cee9f71d35f1eb7397d08b9c908115f598b5
Britta Vieth <britta.vieth@cornelsen.de>
2023-01-09 14:13:40 +0100

    integrated server into repository

---
+++ src/main/java/com/ducky/duckythewizard/server/HighScore.java
@@ -0,0 +1,41 @@
+package com.ducky.duckythewizard.server;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class HighScore {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int score;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
